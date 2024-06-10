7eac806eb5ef0ce78f303a22c5fbc091d8af4243
Clint <81433235+ryjin111@users.noreply.github.com>
2024-06-11 07:52:48 +0800

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,7 +15,7 @@ import {
   Text,
 } from "@chakra-ui/react";
 
-export default function ProfileMenu() {
+export default function ProfileSection() {
   return (
     <Flex>
       <Box mt="24px" m="auto">
