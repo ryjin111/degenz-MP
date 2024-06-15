71f712a34aaeac816c43e11ffefa53d9153575fa
Clint <81433235+ryjin111@users.noreply.github.com>
2024-06-15 19:36:42 +0800

    Update Navbar.tsx

--- src/components/shared/Navbar.tsx
+++ src/components/shared/Navbar.tsx
@@ -46,7 +46,7 @@ export function Navbar() {
             fontWeight="extrabold"
           >
             {/* Replace this with your own branding */}
-            Cavern
+            CAVERN
           </Heading>
         </Box>
         <Box display={{ lg: "block", base: "none" }}>
